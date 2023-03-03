8b94c98b0f16574ac19aae1a73d1a90b97549709
enigma999 <stanborst@gmail.com>
2023-03-03 19:24:26 +0100

    opdracht 5a gemaakt

---
+++ huiswerk week 1/opdracht5_VeiligDelen.py
@@ -0,0 +1,6 @@
+getal1 = int(input("wat is getal 1?"))
+getal2 = int(input("wat is getal 2?"))
+resultaatPlus = getal1 + getal2
+resultaatMin = getal1 - getal2
+resultaatVermenigvuldigen = getal1 + getal2
+resultaatDelendoor = getal1 + getal2
