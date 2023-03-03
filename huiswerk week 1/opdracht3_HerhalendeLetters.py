647e41fcdc61040820b399c4f7cbd62fa6e87a58
enigma999 <stanborst@gmail.com>
2023-03-03 17:20:48 +0100

    afgemaakt

---
+++ huiswerk week 1/opdracht3_HerhalendeLetters.py
@@ -0,0 +1,10 @@
+tekst = "a"
+print(tekst+" "+str(len(tekst)))
+tekst = tekst + tekst
+print(tekst+" "+str(len(tekst)))
+tekst = tekst + tekst
+print(tekst+" "+str(len(tekst)))
+tekst = tekst + tekst
+print(tekst+" "+str(len(tekst)))
+
+
