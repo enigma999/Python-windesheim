f30483c703f4ce2fb814220d6cd8b38051a68cc7
enigma999 <stanborst@gmail.com>
2023-03-03 22:41:33 +0100

    opdracht 7 af

---
+++ huiswerk week 1/opdracht12.py
@@ -0,0 +1,21 @@
+cijfer = input("wat is het cijfer?")
+if(cijfer == 1):
+    print("dat is zeer slecht")
+elif(cijfer == 2):
+    print("dat is slecht")
+elif(cijfer == 3):
+    print("dat is ruim onvoldoende")
+elif(cijfer == 4):
+    print("dat is onvoldoende")
+elif(cijfer == 5):
+    print("dat is zeer matig")
+elif(cijfer == 6):
+    print("dat is voldoende")
+elif(cijfer == 7):
+    print("dat is ruim voldoende")
+elif(cijfer == 8):
+    print("dat is goed")
+elif(cijfer == 9):
+    print("dat is zeer goed")
+elif(cijfer == 10):
+    print("dat is uitstekend")
